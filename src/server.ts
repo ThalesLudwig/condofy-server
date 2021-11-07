6bfee933bd98775586f17ee4ff08c4fca9c9ba20
Thales Ludwig <thales.ludwig@gmail.com>
2021-11-06 23:03:37 -0300

    Adding PORT fallback

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { serverHttp } from "./app";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 serverHttp.listen(PORT, () => console.log(`Running on port ${PORT}`));
